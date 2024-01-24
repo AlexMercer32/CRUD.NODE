6cdd03b6e8c800a5d748d546f392324700cc54c7
Alex Shurganov <shurganovalex@gmail.com>
2024-01-24 22:01:30 +0200

    Try to connect DB

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
-
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+//route files
+const workers = require('./routes/workers');
+//connect to DB
+const db = connectDB;
 //load env vars
 dotenv.config({path:'./config/config.env'});
 
 const app = express();
+
+//mount routers
+app.use('/api/workers',workers);
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,console.log(`Server start on port ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+const server = app.listen(
+    PORT,
+    console.log(`Server start on port ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
