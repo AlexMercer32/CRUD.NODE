561bfa2afab36a73da0e5d007a705ec36aa868fa
Alex Shurganov <shurganovalex@gmail.com>
2024-01-28 11:02:27 +0200

    Connect to db

--- config/db.js
+++ config/db.js
@@ -1,18 +1,24 @@
 const mongoose = require('mongoose');
 
+const dotenv = require('dotenv');
+const {Mongoose} = require("mongoose");
 
+dotenv.config();
+const db = async () => {
+    try {
+        const connection = await mongoose;
+      await connection.connect(process.env.MONGO_URL,{
+          retryWrites: true,
+          w: "majority"
+      })
+        const _mongoose = this instanceof Mongoose ? this : mongoose;
+      const conn = _mongoose.connection;
+        console.log(`MongoDB Connected:${conn.host}`)
+      return conn.openUri('mongodb+srv://shurganovalex:yxvQb4xsVIcAMX7r@alexmercer.nhguebu.mongodb.net/Alex/Alex')
+          .then(()=> _mongoose)
+    } catch (error){
+console.error(error)
+    }
+}
 
-
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
-
-const blogSchema = new Schema({
-id: Number,
-firstName: String,
-   lastName: String,
-enum:[
-    'high',
-    'middle',
-    'low'
-]
-   });
+module.exports = db;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
-//const connectDB = require('./config/db');
+const db = require('./config/db');
+
 //route files
 const workers = require('./routes/workers');
 //connect to DB
@@ -11,9 +12,13 @@ dotenv.config({path:'./config/config.env'});
 const app = express();
 
 //mount routers
-app.use('/api/workers',workers);
 const PORT = process.env.PORT || 3000;
+//connect to db
+db().then(r => {
 
+});
+
+app.use('/api/workers',workers);
  app.listen(
     PORT,
     console.log(`Server start on port ${process.env.NODE_ENV} mode on port ${PORT}`)
